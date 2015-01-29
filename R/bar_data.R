87abd94dcb6db647afecfd7170e1e421863f175f
pgarnry <pg@saxobank.com>
2015-01-29 09:33:35 +0100

    Changed BarData function  documentation.

--- R/bar_data.R
+++ R/bar_data.R
@@ -1,30 +1,42 @@
-
-#' Function getting historical bar data
-#' @param tickers           character vector specifying the tickers
-#' @param type              a character string stating the security class 
-#' such as equities "Equity" or currencies "Curncy". The input is not 
-#' case sensitive
-#' @param start.date.time   a character string specifying the start date 
-#' and time
-#' @param end.date.time     a character string specifying the end date 
-#' and time
-#' @param time.zone         which time zone should be attached to the
-#' specified date and time
-#' @param interval          a character string stating the bar interval 
-#' in minutes. One minute is the highest frequency allowed
+#' Get historical bar data on financial instruments
+#' 
+#' The function works for one or multiple tickers across
+#' many security classes
+#' 
+#' @param tickers Character vector specifying the ticker code(s)
+#' @param type Specify security class such as equities "Equity" or
+#' currencies "Curncy". The input is not case sensitive.
+#' @param start.date A character vector specifying the start date in 
+#' date class format. See details.
+#' @param start.time A character vector specifying the start time.
+#' See details.
+#' @param end.date A character vector specifying the start date in
+#' data class format.
+#' @param end.time A character vector.
+#' @param time.zone The time zone should be attached to the
+#' specified date and time. See details.
+#' @param interval A character vector stating the bar interval in 
+#' minutes. One minute is the highest frequency allowed.
 #' @details In the current version the function has only traded bars as 
 #' its default input. Bid and ask bar data points are possible and 
 #' will be added in the future.
 #' 
-#' The time zone input is added to make it easier to set times as the 
-#' local time for when the exchange is open never changes e.g. always 
+#' The date inputs should follow R's Date Class format "\%Y-\%m-\%d" e.g.
+#' "2014-01-01" for January 1, 2014. The end date's default value is NULL 
+#' which the function interprets as today. Time inputs should the standard 
+#' POSIX format "\%H:\%M:\%S" e.g. "09:30:00".
+#' 
+#' The time zone input is added to make it easier to set times, as the 
+#' local time for when the exchange is open never changes, e.g. always 
 #' 09:30 to 16:00 for NYSE and NASDAQ unless on a holiday, whereas 
 #' GMT changes two times a year because of daylight saving.
 #' 
 #' The data output is a xts object. For multiple tickers the xts 
 #' objects are stored in a list as missing bar data can in some 
 #' cases create unequal length of bar data for each ticker making 
-#' it impossible to wrap the data into an array.
+#' it difficult to wrap the data into an array. It becomes even more 
+#' cumbersome for European equities. The attached time zone is set to 
+#' the system's time zone.
 #' 
 #' As especially stock prices are impacted on ex-dividend dates 
 #' the option "adjustmentFollowDPDF" is to TRUE as default in 
@@ -40,6 +52,8 @@
 #' @references The official time zone names accepted for POSIX are found 
 #' here:
 #' \url{http://en.wikipedia.org/wiki/List_of_tz_database_time_zones}
+#' @examples
+#' BarData()  # Returns six months of 5 minutes bar data on Apple
 
 BarData <- function(tickers = "AAPL US",
                     type = "Equity",
@@ -49,6 +63,8 @@ BarData <- function(tickers = "AAPL US",
                     end.time = NULL,
                     time.zone = "America/New_York",
                     interval = "5") {
+  
+  
   
   utc.start <- paste(start.date, start.time, sep = " ")
   utc.start <- paste(format(with_tz(as.POSIXlt(utc.start,

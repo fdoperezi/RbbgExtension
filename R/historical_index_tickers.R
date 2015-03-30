ec080872fc959aa1539da13ea3ccb08f142f2b93
pgarnry <pg@saxobank.com>
2015-03-30 11:44:43 +0200

    The option available for the HistIndexTickers function allowing the user to specify whether to get composite tickers or ISIN codes out as the instrument identification.

--- R/historical_index_tickers.R
+++ R/historical_index_tickers.R
@@ -1,19 +1,32 @@
-
-#' Function getting historical index tickers to avoid surviourship bias
-#' @param index       a character string specifying the index
+#' Get historical index tickers to avoid surviourship bias
+#' 
+#' The function works for one equity index at a time
+#' 
+#' @param index a character string specifying the index ticker
+#' @param field specifying the output instrument identification
 #' @param startdate   a character string specifying the start date
 #' @param enddate     a character string specifying the end date
 #' @param freq        a character string specifying time frequency
+#' @details The field variable has two options: "ticker" or "isin" 
+#' enabling the function to return the desired instrument identification.
+#' 
+#' The frequency variable is set using the standard Bloomberg inputs such 
+#' as "MONTHLY", "DAILY" etc.
 #' @export
 #' @import Rbbg
 #' @import xts
 
 HistIndexTickers <- function(index = "SPX",
-                             startdate = "20090101",
+                             field = "ticker",
+                             startdate = "20140101",
                              enddate = "",
                              freq = "MONTHLY") {
   
   conn <- blpConnect()
+  
+  field.df <- data.frame(ticker = "COMPOSITE_EXCH_CODE",
+                         isin = "ID_ISIN",
+                         stringsAsFactors = FALSE)
   
   index.prices <- HistData(tickers = index,
                            type = "Index",
@@ -38,15 +51,25 @@ HistIndexTickers <- function(index = "SPX",
                     override_fields = "END_DATE_OVERRIDE",
                     override_values = gsub("[[:punct:]]","",date.seq[i]))
     
-    composite.ticker <- bdp(conn = conn,
-                            securities = paste(bbg.data[, "Index Member"], "Equity"),
-                            fields = "COMPOSITE_EXCH_CODE")
+    field.data <- bdp(conn = conn,
+                      securities = paste(bbg.data[, "Index Member"], "Equity"),
+                      fields = field.df[, field])
     
-    hist.index.tickers.temp[[i]] <- paste(substr(bbg.data[, 1],
-                                                 1,
-                                                 nchar(bbg.data[, 1]) - 2),
-                                          composite.ticker[, 1],
-                                          sep = "")
+    if(field == "ticker") {
+      
+      hist.index.tickers.temp[[i]] <- paste(substr(bbg.data[, 1],
+                                                   1,
+                                                   nchar(bbg.data[, 1]) - 2),
+                                            field.data[, 1],
+                                            sep = "")
+      
+    }
+    
+    if(field == "isin") {
+      
+      hist.index.tickers.temp[[i]] <- field.data[, 1]
+      
+    }
 
   }
   
